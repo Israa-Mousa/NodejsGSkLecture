e824764fb3adffbe4395bbe9fa33f5e9ef1d9b25
Israa Mazaraa <45329110+Israa-Mousa@users.noreply.github.com>
2025-09-14 12:51:11 +0300

    before

--- module/user/user.data.ts
+++ module/user/user.data.ts
@@ -1,33 +1,17 @@
 import { User } from './user.entity';
+import { faker } from '@faker-js/faker';
 
+export function createndomUser(){
+  const randomUser: User = {
+       id: faker.string.uuid(),
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      createdAt:faker.date.past(),
+       password: faker.internet.password(), 
+      updatedAt: faker.date.anytime(),
+      avatar: faker.image.avatar(),
+  }
+  return randomUser;
+}
 
-export const getUserData = ():  User[] => [
-  {
-      id: 'u1',
-      name: 'Alice Johnson',  
-      email: 'alice@example.com',
-      createdAt: new Date('2025-01-01T10:00:00Z'),
-      updatedAt: new Date('2025-01-01T10:00:00Z'),
-      password: 'hashedpassword123',
-    },
-    {
-      id: 'u2',
-      name: 'Bob Smith',
-      email: 'bob@example.com',
-      createdAt: new Date('2025-02-01T12:00:00Z'),
-      updatedAt: new Date('2025-02-01T12:00:00Z'),
-            password: 'hashedpassword123',
-
-    },
-    {
-      id: 'u3',
-      name: 'Charlie Davis',
-      email: 'charlie@example.com',
-      createdAt: new Date('2025-03-01T14:30:00Z'),
-      updatedAt: new Date('2025-03-01T14:30:00Z'),
-            password: 'hashedpassword123'
-
-    }
-];
-
-export const userData= getUserData(); 
+export const userData: User[] = faker.helpers.multiple(createndomUser, { count: 10 });
